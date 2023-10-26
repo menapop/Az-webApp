d914e3b90d1ce619ad25ea45e3a8dcbb43b39c40
Mina Morcos <mina.morcos@cmeoffshore.com>
2023-10-26 13:12:14 +0300

    changes

--- Az-webApp/Pages/Index.cshtml.cs
+++ Az-webApp/Pages/Index.cshtml.cs
@@ -6,15 +6,19 @@ namespace Az_webApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
+
+       
 
         public void OnGet()
         {
-
+            ViewData["Greeting"] = _configuration["Greeting"];
         }
     }
 }
